3a0b9b11aa744dde98fa3a1c2619ec3aef4dcca1
n2d8 <09orrp1@gmail.com>
2014-05-16 17:26:35 -0700

    new test stockwatcher

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.google.gwt.sample.stockwatcher.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface StockServiceAsync {
-  public void addStock(String symbol, AsyncCallback<Void> async);
-  public void removeStock(String symbol, AsyncCallback<Void> async);
-  public void getStocks(AsyncCallback<String[]> async);
-}
